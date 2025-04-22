8fa8c3cec54b3a057c8fee4c2cad447b71365d36
Shohanur Rahman <shohanurr490@gmail.com>
2025-04-22 22:58:05 +0600

    added api for view and create

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from paste lite"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from snippetbox view"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from snippet create"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("Starting server on :4000")
 
